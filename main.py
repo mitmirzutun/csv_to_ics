3ff99c2a71b67f06eeb1f66d7147e29e7903deb7
Milan Cieliebak <milan1111@gmx.de>
2025-02-09 19:46:25 +0100

    Added shell only support

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
 import converter
+TKINTER_SUCESSFUL: bool
+try:
+    import tkinter.filedialog
+    TKINTER_SUCESSFUL = True
+except ModuleNotFoundError:
+    TKINTER_SUCESSFUL = False
 
 
 def main():
-    import tkinter.filedialog
     import os
-    file_name = tkinter.filedialog.askopenfilename(title="Open file",
-                                                   filetypes=[("CSV File", ".csv"),
-                                                              ("Carlender File", ".ics"),
-                                                              ("Exel 2010 File", ".xlsx")])
+    if TKINTER_SUCESSFUL:
+        file_name = tkinter.filedialog.askopenfilename(title="Open file",
+                                                       filetypes=[("CSV File", ".csv"),
+                                                                  ("Carlender File", ".ics"),
+                                                                  ("Exel 2010 File", ".xlsx")])
+    else:
+        file_name = input("File name: ")
     if file_name.endswith(".ics"):
-        save_file_name = os.path.basename(file_name)
-        save_file_name = ".".join(save_file_name.split(".")[:-1]) + ".csv"
-        save_file_name = tkinter.filedialog.asksaveasfilename(defaultextension="csv",
-                                                              filetypes=[("CSV Files", ".csv")],
-                                                              title="Save file as",
-                                                              initialfile=save_file_name)
+        initialfile = os.path.basename(file_name)
+        initialfile = ".".join(initialfile.split(".")[:-1]) + ".csv"
+        save_file_name: str
+        if TKINTER_SUCESSFUL:
+            save_file_name = tkinter.filedialog.asksaveasfilename(defaultextension="csv",
+                                                                  filetypes=[("CSV Files", ".csv")],
+                                                                  title="Save file as",
+                                                                  initialfile=initialfile)
+        else:
+            tmp = input(f".csv file name (default {initialfile}):")
+            save_file_name = initialfile if tmp == "" else tmp
         converter.ics_to_csv(file_name, save_file_name)
     if file_name.endswith(".xlsx"):
         initialfile = os.path.basename(file_name)
         initialfile = ".".join(initialfile.split(".")[:-1]) + ".ics"
-        save_file_name = tkinter.filedialog.asksaveasfilename(defaultextension="ics",
-                                                              filetypes=[("Carlender File", ".ics")],
-                                                              title="Save file as",
-                                                              initialfile=initialfile)
+        save_file_name: str
+        if TKINTER_SUCESSFUL:
+            save_file_name = tkinter.filedialog.asksaveasfilename(defaultextension="ics",
+                                                                  filetypes=[("Carlender File", ".ics")],
+                                                                  title="Save file as",
+                                                                  initialfile=initialfile)
+        else:
+            tmp = input(f".ics file name (default {initialfile}):")
+            save_file_name = initialfile if tmp == "" else tmp
         converter.xls_to_ics(file_name, save_file_name)
 
 
