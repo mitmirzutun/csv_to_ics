f5dcd01d01083a12c0ddd218d58cc32fa2233299
Milan Cieliebak <milan1111@gmx.de>
2025-02-09 18:15:21 +0100

    using tkinter

--- converter.py
+++ converter.py
@@ -5,16 +5,24 @@ def ics_to_csv(ics_file_name,csv_file_name):
     convert.make_csv()
     convert.save_csv(csv_file_name)
 def xls_to_ics(xlsx_file_name,ics_file_name):
+    XLSX_BOOKING_ID:int=1
+    XLSX_START_DATE:int=21
+    XLSX_START_TIME:int=22
+    XLSX_END_DATE:int=23
+    XLSX_END_TIME:int=24
+    XLSX_DESCRIPTION:int=31
+    XLSX:int=10
     import openpyxl,csv,datetime
     xlsx_file=openpyxl.load_workbook(xlsx_file_name)
     worksheet=xlsx_file.active
     with open(ics_file_name+".tmp.csv","w") as csv_file:
         writer=csv.writer(csv_file)
+        input(worksheet.rows)
         for row in range(2,worksheet.max_row+1):
             print(worksheet.cell(column=1,row=row).value)
             if worksheet.cell(column=17,row=row).value!="completed":
                 continue
-            booking_id=worksheet.cell(column=1,row=row).value
+            booking_id=worksheet.cell(column=XLSX_BOOKING_ID,row=row).value
             start_date=worksheet.cell(column=21,row=row).value+" "+worksheet.cell(column=22,row=row).value
             end_date=worksheet.cell(column=23,row=row).value+" "+worksheet.cell(column=24,row=row).value
             desc=worksheet.cell(column=31,row=row).value
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import converter
 def main():
-    file_name=input("File name: ")
+    import tkinter.filedialog
+    import os
+    file_name=tkinter.filedialog.askopenfilename(title="Open file",filetypes=[("CSV Files",".csv"),("Carlender File",".ics"),("",".xlsx")])
     if file_name.endswith(".ics"):
-        converter.ics_to_csv(file_name,input("CSV file name: "))
+        save_file_name = os.path.basename(file_name)
+        save_file_name = ".".join(save_file_name.split(".")[:-1])+".csv"
+        save_file_name = tkinter.filedialog.asksaveasfilename(defaultextension="csv",filetypes=[("CSV Files",".csv")],title="Save file as",initialfile=save_file_name)
+        converter.ics_to_csv(file_name,save_file_name)
     if file_name.endswith(".xlsx"):
-        converter.xls_to_ics(file_name,input("ICS file name: "))
+        save_file_name = os.path.basename(file_name)
+        save_file_name = ".".join(save_file_name.split(".")[:-1])+".ics"
+        save_file_name = tkinter.filedialog.asksaveasfilename(defaultextension="ics",filetypes=[("Carlender File",".ics")],title="Save file as",initialfile=save_file_name)
+        converter.xls_to_ics(file_name,save_file_name)
 if __name__=="__main__":
     main()
