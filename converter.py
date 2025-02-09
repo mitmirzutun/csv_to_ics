d06e292a438e64600dc95775d90a498f6b0591b5
Milan Cieliebak <milan1111@gmx.de>
2025-02-09 18:55:07 +0100

    formatted code

--- converter.py
+++ converter.py
@@ -1,43 +1,62 @@
 import csv_ical
-convert=csv_ical.Convert()
-def ics_to_csv(ics_file_name,csv_file_name):
+convert = csv_ical.Convert()
+
+
+def ics_to_csv(ics_file_name, csv_file_name):
     convert.read_ical(ics_file_name)
     convert.make_csv()
     convert.save_csv(csv_file_name)
-def xls_to_ics(xlsx_file_name,ics_file_name):
-    XLSX_BOOKING_ID:int=1
-    XLSX_START_DATE:int=21
-    XLSX_START_TIME:int=22
-    XLSX_END_DATE:int=23
-    XLSX_END_TIME:int=24
-    XLSX_DESCRIPTION:int=31
-    XLSX:int=10
-    import openpyxl,csv,datetime
-    xlsx_file=openpyxl.load_workbook(xlsx_file_name)
-    worksheet=xlsx_file.active
-    with open(ics_file_name+".tmp.csv","w") as csv_file:
-        writer=csv.writer(csv_file)
-        input(next(worksheet.rows))
-        for row in range(2,worksheet.max_row+1):
-            print(worksheet.cell(column=1,row=row).value)
-            if worksheet.cell(column=17,row=row).value!="completed":
+
+
+def xls_to_ics(xlsx_file_name, ics_file_name):
+    import openpyxl
+    import csv
+    import datetime
+    xlsx_file = openpyxl.load_workbook(xlsx_file_name)
+    worksheet = xlsx_file.active
+    with open(ics_file_name + ".tmp.csv", "w") as csv_file:
+        writer = csv.writer(csv_file)
+        titles = list(val.value for val in next(worksheet.rows))
+        XLSX_BOOKING_ID: int = titles.index("bookingCode")
+        XLSX_INTERNAL: int = titles.index("price.groupInternal")
+        XLSX_STATUS: int = titles.index("status")
+        XLSX_PERSONS: int = titles.index("persons")
+        XLSX_RESOURCE: int = titles.index("resourceTitle")
+        XLSX_START_DATE: int = titles.index("bookingStartAtDate")
+        XLSX_START_TIME: int = titles.index("bookingStartAtTime")
+        XLSX_END_DATE: int = titles.index("bookingEndAtDate")
+        XLSX_END_TIME: int = titles.index("bookingEndAtTime")
+        XLSX_DESCRIPTION: int = titles.index("customerNote")
+        for row in range(2, worksheet.max_row + 1):
+            print(worksheet.cell(column=XLSX_BOOKING_ID, row=row).value)
+            input(worksheet.cell(column=XLSX_STATUS,row=row).value)
+            input(XLSX_STATUS)
+            if worksheet.cell(column=XLSX_STATUS, row=row).value != "completed":
                 continue
-            booking_id=worksheet.cell(column=XLSX_BOOKING_ID,row=row).value
-            start_date=worksheet.cell(column=21,row=row).value+" "+worksheet.cell(column=22,row=row).value
-            end_date=worksheet.cell(column=23,row=row).value+" "+worksheet.cell(column=24,row=row).value
-            desc=worksheet.cell(column=31,row=row).value
-            writer.writerow([booking_id,start_date,end_date,desc,"Freihampton"])
-    config={
-        'CSV_NAME':0,
-        'CSV_START_DATE':1,
-        'CSV_END_DATE':2,
-        'CSV_DESCRIPTION':3,
-        'CSV_LOCATION':4,
-        'CSV_DELIMITER':","
+            input(worksheet.cell(column=XLSX_RESOURCE,row=row).value)
+            if worksheet.cell(column=XLSX_RESOURCE,row=row).value!="Pavillon Gemeinschaftsraum - Kooperative Großstadt ":
+                continue
+            booking_id = worksheet.cell(column=XLSX_BOOKING_ID, row=row).value
+            if worksheet.cell(column=XLSX_INTERNAL, row=row).value=="TRUE":
+                booking_id += " (INTERNAL)"
+            persons = worksheet.cell(column=XLSX_PERSONS, row=row).value
+            start_date = worksheet.cell(column=XLSX_START_DATE, row=row).value + " " + worksheet.cell(column=XLSX_START_TIME, row=row).value
+            end_date = worksheet.cell(column=XLSX_END_DATE, row=row).value + " " + worksheet.cell(column=XLSX_END_TIME, row=row).value
+            desc = worksheet.cell(column=XLSX_DESCRIPTION, row=row).value
+            writer.writerow([booking_id, start_date, end_date, desc, "Freihampton", persons])
+            input([booking_id, start_date, end_date, desc, "Freihampton", persons])
+    config = {
+        'CSV_NAME': 0,
+        'CSV_START_DATE': 1,
+        'CSV_END_DATE': 2,
+        'CSV_DESCRIPTION': 3,
+        'CSV_LOCATION': 4,
+        'CSV_PERSONS': 5,
+        'CSV_DELIMITER': ","
     }
-    convert.read_csv(ics_file_name+".tmp.csv",config)
+    convert.read_csv(ics_file_name + ".tmp.csv", config)
     for i in range(len(convert.csv_data)):
-        convert.csv_data[i][config["CSV_START_DATE"]]=datetime.datetime.strptime(convert.csv_data[i][config["CSV_START_DATE"]], "%Y-%m-%d %H:%M")
-        convert.csv_data[i][config["CSV_END_DATE"]]=datetime.datetime.strptime(convert.csv_data[i][config["CSV_END_DATE"]], "%Y-%m-%d %H:%M")
+        convert.csv_data[i][config["CSV_START_DATE"]] = datetime.datetime.strptime(convert.csv_data[i][config["CSV_START_DATE"]], "%Y-%m-%d %H:%M")
+        convert.csv_data[i][config["CSV_END_DATE"]] = datetime.datetime.strptime(convert.csv_data[i][config["CSV_END_DATE"]], "%Y-%m-%d %H:%M")
     convert.make_ical(config)
     convert.save_ical(ics_file_name)
--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 import converter
+
+
 def main():
     import tkinter.filedialog
     import os
-    file_name=tkinter.filedialog.askopenfilename(title="Open file",filetypes=[("CSV Files",".csv"),("Carlender File",".ics"),("",".xlsx")])
+    if False:
+        file_name = tkinter.filedialog.askopenfilename(title="Open file",
+                                                       filetypes=[("CSV File", ".csv"),
+                                                                  ("Carlender File", ".ics"),
+                                                                  ("Exel 2010 File", ".xlsx")])
+    else:
+        file_name="data/Klink Buchungen 2025.xlsx"
     if file_name.endswith(".ics"):
         save_file_name = os.path.basename(file_name)
         save_file_name = ".".join(save_file_name.split(".")[:-1])+".csv"
-        save_file_name = tkinter.filedialog.asksaveasfilename(defaultextension="csv",filetypes=[("CSV Files",".csv")],title="Save file as",initialfile=save_file_name)
-        converter.ics_to_csv(file_name,save_file_name)
+        save_file_name = tkinter.filedialog.asksaveasfilename(defaultextension="csv",
+                                                              filetypes=[("CSV Files", ".csv")],
+                                                              title="Save file as",
+                                                              initialfile=save_file_name)
+        converter.ics_to_csv(file_name, save_file_name)
     if file_name.endswith(".xlsx"):
-        save_file_name = os.path.basename(file_name)
-        save_file_name = ".".join(save_file_name.split(".")[:-1])+".ics"
-        save_file_name = tkinter.filedialog.asksaveasfilename(defaultextension="ics",filetypes=[("Carlender File",".ics")],title="Save file as",initialfile=save_file_name)
-        converter.xls_to_ics(file_name,save_file_name)
-if __name__=="__main__":
+        initialfile = os.path.basename(file_name)
+        initialfile = ".".join(initialfile.split(".")[:-1])+".ics"
+        if False:
+            save_file_name = tkinter.filedialog.asksaveasfilename(defaultextension="ics",
+                                                                  filetypes=[("Carlender File",
+                                                                              ".ics")],
+                                                                  title="Save file as",
+                                                                  initialfile=initialfile)
+        else:
+            save_file_name="Klink Buchungen 2025.ics"
+        converter.xls_to_ics(file_name, save_file_name)
+
+
+if __name__ == "__main__":
     main()
