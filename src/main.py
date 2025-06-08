45ccc1ae32512c9ab2ed0b04059bd366b81d7f97
Suny Kim <ynux@gmx.net>
2025-06-08 19:46:32 +0200

    Suny simplifying

--- src/main.py
+++ src/main.py
@@ -1,69 +1,29 @@
 import converter
-TKINTER_SUCESSFUL: bool
-try:
-    import tkinter.filedialog
-    TKINTER_SUCESSFUL = True
-except ModuleNotFoundError:
-    TKINTER_SUCESSFUL = False
-
 
 def main():
     import os
-    if TKINTER_SUCESSFUL:
-        file_name = (tkinter.filedialog
-                     .askopenfilename(title="Open file",
-                                      filetypes=[("CSV File", ".csv"),
-                                                 ("Carlender File", ".ics"),
-                                                 ("Exel 2010 File", ".xlsx")]))
-    else:
-        file_name = input("File name: ")
+    file_name = input("File name: ")
+    save_file_name: str
     if file_name.endswith(".ics"):
         initialfile = os.path.basename(file_name)
         initialfile = ".".join(initialfile.split(".")[:-1]) + ".csv"
-        save_file_name: str
-        if TKINTER_SUCESSFUL:
-            save_file_name = (tkinter.filedialog
-                              .asksaveasfilename(defaultextension="csv",
-                                                 filetypes=[("CSV Files",
-                                                             ".csv")],
-                                                 title="Save file as",
-                                                 initialfile=initialfile))
-        else:
-            tmp = input(f".csv file name (default {initialfile}):")
-            save_file_name = initialfile if tmp == "" else tmp
+        tmp = input(f".csv file name (default {initialfile}):")
+        save_file_name = initialfile if tmp == "" else tmp
         converter.ics_to_csv(file_name, save_file_name)
     if file_name.endswith(".xlsx"):
         initialfile = os.path.basename(file_name)
         initialfile = ".".join(initialfile.split(".")[:-1]) + ".ics"
-        save_file_name: str
-        if TKINTER_SUCESSFUL:
-            save_file_name = (tkinter.filedialog
-                              .asksaveasfilename(defaultextension="ics",
-                                                 filetypes=[("Carlender File",
-                                                             ".ics")],
-                                                 title="Save file as",
-                                                 initialfile=initialfile))
-        else:
-            tmp = input(f".ics file name (default {initialfile}):")
-            save_file_name = initialfile if tmp == "" else tmp
+        tmp = input(f".ics file name (default {initialfile}):")
+        save_file_name = initialfile if tmp == "" else tmp
         converter.xls_to_ics(file_name, save_file_name)
     if file_name.endswith(".csv"):
         initialfile = os.path.basename(file_name)
+        outputdir = initialfile.split(".")[:-1] 
         initialfile = ".".join(initialfile.split(".")[:-1]) + ".ics"
-        save_file_name: str
-        if TKINTER_SUCESSFUL:
-            save_file_name = (tkinter.filedialog
-                              .asksaveasfilename(defaultextension="ics",
-                                                 filetypes=[("Carlender File",
-                                                             ".ics")],
-                                                 title="Save file as",
-                                                 initialfile=initialfile))
-        else:
-            tmp = input(f".ics file name (default {initialfile}):")
-            save_file_name = initialfile if tmp == "" else tmp
+        tmp = input(f".ics file name (default {initialfile}):")
+        save_file_name = initialfile if tmp == "" else tmp
         calender = converter.OVBuilder().from_path(file_name)
         calender.to_ical(save_file_name)
-
 
 if __name__ == "__main__":
     main()
